dc118589bbf8692a3a9b6fd3cd2d3c471e6bfca0
Igor Lopes <claudioigorai@gmail.com>
2023-05-10 21:28:53 -0300

    Inserção de busca dos games por Id

--- src/main/java/com/seasontech/dslist/controller/GameController.java
+++ src/main/java/com/seasontech/dslist/controller/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.seasontech.dslist.dto.GameDTO;
 import com.seasontech.dslist.dto.GameMinDTO;
 import com.seasontech.dslist.service.GameService;
 
@@ -16,6 +18,11 @@ public class GameController {
     @Autowired
     private GameService gameService;
 
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id){
+        GameDTO result = gameService.findById(id);
+        return result;
+    }
 
     @GetMapping
     public List<GameMinDTO> findAll(){
