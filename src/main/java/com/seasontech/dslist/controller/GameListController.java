38432197eaaea8c759a15d2ed1fb32e7b837b449
Igor Lopes <claudioigorai@gmail.com>
2023-05-11 22:33:57 -0300

    Criação de endpoint que atualiza a posição no banco de dados

--- src/main/java/com/seasontech/dslist/controller/GameListController.java
+++ src/main/java/com/seasontech/dslist/controller/GameListController.java
@@ -5,11 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.seasontech.dslist.dto.GameListDTO;
 import com.seasontech.dslist.dto.GameMinDTO;
+import com.seasontech.dslist.dto.ReplacementDTO;
 import com.seasontech.dslist.service.GameListService;
 import com.seasontech.dslist.service.GameService;
 
@@ -30,5 +33,9 @@ public class GameListController {
     public List<GameMinDTO> findByList(@PathVariable Long listId){
         List<GameMinDTO> result = gameService.findByList(listId);
         return result;
+    }
+    @PostMapping(value = "/{listId}/replacement")
+    public void move(@PathVariable Long listId, @RequestBody ReplacementDTO body){
+        gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex());
     }
 }
